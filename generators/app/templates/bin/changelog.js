5c0a671a6b490af7d0e92ab5d0821a4e9df78722
Yoann Moinet <yoann.moinet@gmail.com>
2015-08-05 09:40:52 -0400

    feat: commit what's changed in the changelog

--- generators/app/templates/bin/changelog.js
+++ generators/app/templates/bin/changelog.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var conventionalChangelog = require('conventional-changelog');
 var changelogFile = fs.createWriteStream('CHANGELOG.md');
+var exec = require('child_process').exec;
 
 conventionalChangelog({
 	preset: 'angular'
@@ -10,3 +11,15 @@ conventionalChangelog({
 	from: '',
 	to: 'HEAD'
 }).pipe(changelogFile);
+
+// Commit what's changed in the changelog.
+changelogFile.on('unpipe', function (src) {
+    console.log('commiting changes to CHANGELOG.md');
+    exec('git add CHANGELOG.md && git commit -m "docs: changelog"', function (err) {
+        if (!err) {
+            process.exit(0);
+        }
+        console.error(err);
+        process.exit(1);
+    });
+});
